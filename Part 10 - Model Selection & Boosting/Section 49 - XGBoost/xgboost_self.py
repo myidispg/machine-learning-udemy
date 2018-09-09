37dd8bf6161ffe0c356453568bab5df3a44a5839
myidispg <36075176+myidispg@users.noreply.github.com>
2018-09-09 18:57:28 +0530

    Completed XGBoost

--- Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xgboost_self.py
+++ Part 10 - Model Selection & Boosting/Section 49 - XGBoost/xgboost_self.py
@@ -36,3 +36,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from xgboost import XGBClassifier
 classifier = XGBClassifier()
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+from sklearn.model_selection import cross_val_score
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10)
+accuracies.mean()
+accuracies.std()
